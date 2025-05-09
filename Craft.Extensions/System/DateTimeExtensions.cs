d8c8b3773af539029e42eacd8597f83825e4c4df
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-05-09 15:08:59 +0530

    Add Craft.Extensions and Craft.Extensions.Tests projects
    Updated solution file to include new projects for Craft.Extensions and Craft.Extensions.Tests, along with an .editorconfig file. Created project files targeting .NET 10.0 with necessary properties. Added ListExtensions and DateTimeExtensions classes with utility methods for list and date manipulations. Included testing project with references to required libraries.

---
+++ Craft.Extensions/System/DateTimeExtensions.cs
@@ -0,0 +1,38 @@
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace System;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+public static class DateTimeExtensions
+{
+    public static DateTime ClearTime(this DateTime dateTime)
+    {
+        return dateTime.Subtract(new TimeSpan(0,
+            dateTime.Hour,
+            dateTime.Minute,
+            dateTime.Second,
+            dateTime.Millisecond));
+    }
+
+    public static bool IsWeekday(this DayOfWeek dayOfWeek)
+    {
+        return dayOfWeek.IsIn(DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday);
+    }
+
+    public static bool IsWeekend(this DayOfWeek dayOfWeek)
+    {
+        return dayOfWeek.IsIn(DayOfWeek.Saturday, DayOfWeek.Sunday);
+    }
+
+    public static DateTime? SetKindUtc(this DateTime? dateTime)
+    {
+        return dateTime.HasValue ? dateTime.Value.SetKindUtc() : null;
+    }
+
+    public static DateTime SetKindUtc(this DateTime dateTime)
+    {
+        if (dateTime.Kind == DateTimeKind.Utc)
+            return dateTime;
+
+        return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+    }
+}
