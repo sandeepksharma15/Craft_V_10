c4ee344353c743a5297abe9814e3ef5f474a9cd6
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-05-16 11:51:21 +0530

    Add ObjectExtensions class with utility methods
    Introduce a new static class `ObjectExtensions` in `ObjectExtensions.cs` that provides extension methods for safe type casting (`AsType<T>`), conditional execution of functions or actions (`If<T>`), and conversion to specified value types (`ToValue<T>`). Update `ObjectTests.cs` with unit tests to ensure the correctness of these methods.

---
+++ Craft.Extensions/System/ObjectExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System.Globalization;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace System;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+public static class ObjectExtensions
+{
+    public static T AsType<T>(this object obj) where T : class => (T)obj;
+
+    /// <summary>
+    /// Can be used to conditionally perform a function
+    /// on an object and return the modified or the original object.
+    /// It is useful for chained calls.
+    /// </summary>
+    /// <param name="obj">An object</param>
+    /// <param name="condition">A condition</param>
+    /// <param name="func">A function that is executed only if the condition is <c>true</c></param>
+    /// <typeparam name="T">Type of the object</typeparam>
+    /// <returns>
+    /// Returns the modified object (by the <paramref name="func"/> if the <paramref name="condition"/> is <c>true</c>)
+    /// or the original object if the <paramref name="condition"/> is <c>false</c>
+    /// </returns>
+    public static T If<T>(this T obj, bool condition, Func<T, T> func)
+    {
+        if (condition && func is not null)
+            return func(obj);
+
+        return obj;
+    }
+
+    /// <summary>
+    /// Can be used to conditionally perform an action
+    /// on an object and return the original object.
+    /// It is useful for chained calls on the object.
+    /// </summary>
+    /// <param name="obj">An object</param>
+    /// <param name="condition">A condition</param>
+    /// <param name="action">An action that is executed only if the condition is <c>true</c></param>
+    /// <typeparam name="T">Type of the object</typeparam>
+    /// <returns>
+    /// Returns the original object.
+    /// </returns>
+    public static T If<T>(this T obj, bool condition, Action<T> action)
+    {
+        if (condition && action is not null)
+            action(obj);
+
+        return obj;
+    }
+
+    /// <summary>
+    /// Converts an object to a value of the specified type <typeparamref name="T"/>.
+    /// Handles special cases like converting to GUID. Returns the default value if the object is null.
+    /// Uses culture-specific conversion for non-GUID types.
+    /// </summary>
+    /// <typeparam name="T">The target value type.</typeparam>
+    /// <param name="obj">The object to be converted.</param>
+    /// <returns>The converted value of type <typeparamref name="T"/>.</returns>
+    public static T ToValue<T>(this object obj) where T : struct
+    {
+        if (obj is null)
+            return default;
+
+        if (typeof(T) == typeof(Guid) && Guid.TryParse(obj.ToString(), out Guid guid))
+            return (T)(ValueType)guid;
+
+        return obj is IConvertible convertible
+            ? (T)Convert.ChangeType(convertible, typeof(T), CultureInfo.CurrentCulture)
+            : default;
+    }
+}
---
+++ Tests/Craft.Extensions.Tests/System/ObjectTests.cs
@@ -0,0 +1,167 @@
+﻿namespace Craft.Extensions.Tests.System;
+
+public class ObjectTests
+{
+    [Fact]
+    public void AsType_Should_Return_Correct_Type()
+    {
+        // Arrange
+        object obj = "Hello, World!";
+
+        // Act
+        var result = obj.AsType<string>();
+
+        // Assert
+        Assert.IsType<string>(result);
+        Assert.Equal("Hello, World!", result);
+    }
+
+    [Fact]
+    public void AsType_Should_Return_Null_For_Incorrect_Type()
+    {
+        // Arrange
+        object obj = 123;
+
+        // Act & Assert
+        Assert.Throws<InvalidCastException>(() => obj.AsType<string>());
+    }
+
+    [Fact]
+    public void If_Should_Apply_Function_When_Condition_Is_True()
+    {
+        // Arrange
+        const int value = 5;
+        const bool condition = true;
+
+        // Act
+        var result = value.If(condition, x => x * 2);
+
+        // Assert
+        Assert.Equal(10, result);
+    }
+
+    [Fact]
+    public void If_Should_Not_Apply_Function_When_Condition_Is_False()
+    {
+        // Arrange
+        const int value = 5;
+        const bool condition = false;
+
+        // Act
+        var result = value.If(condition, x => x * 2);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void If_Should_Not_Perform_Action_When_Action_Is_Null()
+    {
+        // Arrange
+        const int value = 5;
+        const bool condition = true;
+
+        // Act
+        int newValue = value.If(condition, null!);
+
+        // Assert
+        Assert.Equal(value, newValue);
+    }
+
+    [Fact]
+    public void If_Should_Not_Perform_Action_When_Condition_Is_False()
+    {
+        // Arrange
+        const int value = 5;
+        const bool condition = false;
+        int newValue = 0;
+
+        // Act
+        value.If(condition, x => newValue = x * 2);
+
+        // Assert
+        Assert.Equal(0, newValue);
+    }
+
+    [Fact]
+    public void If_Should_Perform_Action_When_Condition_Is_True()
+    {
+        // Arrange
+        const int value = 5;
+        const bool condition = true;
+        int newValue = 0;
+
+        // Act
+        value.If(condition, x => newValue = x * 2);
+
+        // Assert
+        Assert.Equal(10, newValue);
+    }
+
+    [Fact]
+    public void If_Should_Return_Same_Value_When_Condition_Is_False_And_No_Action_Provided()
+    {
+        // Arrange
+        const string text = "Hello";
+        const bool condition = false;
+
+        // Act
+        var result = text.If(condition, null);
+
+        // Assert
+        Assert.Equal(text, result);
+    }
+
+    [Fact]
+    public void If_Should_Return_Same_Value_When_Condition_Is_True_And_No_Function_Provided()
+    {
+        // Arrange
+        const string text = "Hello";
+        const bool condition = true;
+
+        // Act
+        var result = text.If(condition, null);
+
+        // Assert
+        Assert.Equal(text, result);
+    }
+
+    [Fact]
+    public void ToValue_Should_Convert_Guid_Correctly()
+    {
+        // Arrange
+        object obj = "6F9619FF-8B86-D011-B42D-00C04FC964FF";
+
+        // Act
+        var result = obj.ToValue<Guid>();
+
+        // Assert
+        Assert.Equal(Guid.Parse("6F9619FF-8B86-D011-B42D-00C04FC964FF"), result);
+    }
+
+    [Fact]
+    public void ToValue_Should_Convert_To_Correct_Type()
+    {
+        // Arrange
+        object obj = "123";
+
+        // Act
+        var result = obj.ToValue<int>();
+
+        // Assert
+        Assert.Equal(123, result);
+    }
+
+    [Fact]
+    public void ToValue_Should_Throw_Exception_For_Invalid_Conversion()
+    {
+        // Arrange
+        object obj = "InvalidNumber";
+
+        // Act
+        var action = new Action(() => obj.ToValue<int>());
+
+        // Act & Assert
+        Assert.Throws<FormatException>(() => obj.ToValue<int>());
+    }
+}
