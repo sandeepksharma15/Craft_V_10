5ed218d1a51848df66a1924e55e3224970183875
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-06-30 08:35:32 +0530

    Enhance ParameterReplacerVisitor with documentation
    Updated the `ParameterReplacerVisitor` class to include XML documentation comments that describe its functionality. Changed the class visibility from `internal` to `public` to allow access outside its original assembly.

--- Craft.QuerySpec/Extensions/ParameterReplacerVisitor.cs
+++ Craft.QuerySpec/Extensions/ParameterReplacerVisitor.cs
@@ -2,7 +2,13 @@
 
 namespace Craft.QuerySpec;
 
-internal sealed class ParameterReplacerVisitor : ExpressionVisitor
+/// <summary>
+/// Provides functionality to replace occurrences of a specific parameter in an expression tree with a new expression.
+/// </summary>
+/// <remarks>This class is used internally to modify expression trees by substituting a specified parameter with a
+/// given expression. It is particularly useful in scenarios where dynamic expression manipulation is required, such as
+/// building LINQ queries or transforming lambda expressions.</remarks>
+public sealed class ParameterReplacerVisitor : ExpressionVisitor
 {
     private readonly Expression newExpression;
     private readonly ParameterExpression oldParameter;
