7729865602e09531ad5c28dbf99bfa54114a0dbf
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-07-10 16:47:45 +0530

    Refactor JSON converter for improved clarity and functionality
    - Removed unused `System.Reflection` directive and added `System.Text.Json.Serialization`.
    - Moved `CanConvert` method to the end of the class with added documentation.
    - Enhanced `Read` and `Write` methods with detailed summary comments and switched to a new `CreateLocalOptions` method for local `JsonSerializerOptions` creation.
    - Introduced `CreateLocalOptions` to manage shallow copies of `JsonSerializerOptions`, ensuring controlled serialization behavior.

--- Craft.QuerySpec/Builders/EntityFilterBuilderJsonConverter.cs
+++ Craft.QuerySpec/Builders/EntityFilterBuilderJsonConverter.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Craft.QuerySpec;
@@ -10,18 +9,21 @@ namespace Craft.QuerySpec;
 /// <typeparam name="T">The entity type.</typeparam>
 public class EntityFilterBuilderJsonConverter<T> : JsonConverter<EntityFilterBuilder<T>> where T : class
 {
-    /// <inheritdoc />
-    public override bool CanConvert(Type objectType)
-        => objectType == typeof(EntityFilterBuilder<T>);
-
-    /// <inheritdoc />
+    /// <summary>
+    /// Reads and converts the JSON to an <see cref="EntityFilterBuilder{T}"/> instance.
+    /// </summary>
+    /// <param name="reader">The reader.</param>
+    /// <param name="typeToConvert">The type to convert.</param>
+    /// <param name="options">The serializer options.</param>
+    /// <returns>The deserialized <see cref="EntityFilterBuilder{T}"/>.</returns>
+    /// <exception cref="JsonException">Thrown if the JSON format is invalid.</exception>
     public override EntityFilterBuilder<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType != JsonTokenType.StartArray)
             throw new JsonException($"Invalid format for {nameof(EntityFilterBuilder<>)}: expected array of {nameof(EntityFilterCriteria<>)}.");
 
         var entityFilterBuilder = new EntityFilterBuilder<T>();
-        var localOptions = options.GetClone();
+        var localOptions = CreateLocalOptions(options);
         localOptions.Converters.Add(new EntityFilterCriteriaJsonConverter<T>());
 
         while (reader.Read())
@@ -29,7 +31,7 @@ public class EntityFilterBuilderJsonConverter<T> : JsonConverter<EntityFilterBui
             if (reader.TokenType == JsonTokenType.EndArray)
                 break;
 
-            var entityFilterCriteria = JsonSerializer.Deserialize<EntityFilterCriteria<T>>(ref reader, localOptions) 
+            var entityFilterCriteria = JsonSerializer.Deserialize<EntityFilterCriteria<T>>(ref reader, localOptions)
                 ?? throw new JsonException($"Invalid format for {nameof(EntityFilterBuilder<>)}: expected array of {nameof(EntityFilterCriteria<>)}.");
 
             entityFilterBuilder.EntityFilterList.Add(entityFilterCriteria);
@@ -38,7 +40,13 @@ public class EntityFilterBuilderJsonConverter<T> : JsonConverter<EntityFilterBui
         return entityFilterBuilder;
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    /// Writes the <see cref="EntityFilterBuilder{T}"/> to JSON.
+    /// </summary>
+    /// <param name="writer">The writer.</param>
+    /// <param name="value">The value to write.</param>
+    /// <param name="options">The serializer options.</param>
+    /// <exception cref="ArgumentNullException">Thrown if writer or value is null.</exception>
     public override void Write(Utf8JsonWriter writer, EntityFilterBuilder<T> value, JsonSerializerOptions options)
     {
         ArgumentNullException.ThrowIfNull(writer);
@@ -46,12 +54,54 @@ public class EntityFilterBuilderJsonConverter<T> : JsonConverter<EntityFilterBui
 
         writer.WriteStartArray();
 
-        var localOptions = options.GetClone();
+        var localOptions = CreateLocalOptions(options);
         localOptions.Converters.Add(new EntityFilterCriteriaJsonConverter<T>());
 
         foreach (var entityFilter in value.EntityFilterList)
+        {
             JsonSerializer.Serialize(writer, entityFilter, localOptions);
+        }
 
         writer.WriteEndArray();
+    }
+
+    /// <summary>
+    /// Indicates whether this converter can convert the specified object type.
+    /// </summary>
+    /// <param name="objectType">The type to check.</param>
+    /// <returns>True if the type is <see cref="EntityFilterBuilder{T}"/>; otherwise, false.</returns>
+    public override bool CanConvert(Type objectType)
+        => objectType == typeof(EntityFilterBuilder<T>);
+
+    /// <summary>
+    /// Creates a shallow copy of the provided <see cref="JsonSerializerOptions"/> with selected properties copied.
+    /// </summary>
+    /// <param name="options">The options to copy from.</param>
+    /// <returns>A new <see cref="JsonSerializerOptions"/> instance.</returns>
+    private static JsonSerializerOptions CreateLocalOptions(JsonSerializerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        var clone = new JsonSerializerOptions
+        {
+            AllowTrailingCommas = options.AllowTrailingCommas,
+            DefaultBufferSize = options.DefaultBufferSize,
+            DictionaryKeyPolicy = options.DictionaryKeyPolicy,
+            DefaultIgnoreCondition = options.DefaultIgnoreCondition,
+            IgnoreReadOnlyFields = options.IgnoreReadOnlyFields,
+            IgnoreReadOnlyProperties = options.IgnoreReadOnlyProperties,
+            MaxDepth = options.MaxDepth,
+            NumberHandling = options.NumberHandling,
+            PropertyNameCaseInsensitive = options.PropertyNameCaseInsensitive,
+            PropertyNamingPolicy = options.PropertyNamingPolicy,
+            ReadCommentHandling = options.ReadCommentHandling,
+            ReferenceHandler = options.ReferenceHandler,
+            TypeInfoResolver = options.TypeInfoResolver,
+            UnknownTypeHandling = options.UnknownTypeHandling,
+            WriteIndented = options.WriteIndented
+        };
+
+        // Do not copy converters, as we want to add only the required one.
+        return clone;
     }
 }
