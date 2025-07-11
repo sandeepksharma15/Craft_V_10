a8c5e93116deec0db3e68db41fd79cca4726e82d
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-07-11 09:22:53 +0530

    Refactor core interfaces and enhance unit tests
    Significant refactoring of the codebase, focusing on `IEntityController`, `IHttpService`, `IQuery`, and related classes/interfaces. Redundant methods were removed, and new interfaces were introduced to improve clarity and maintainability.
    
    The `IHttpService` interface was simplified, while `IQuery` gained new methods for advanced querying. Updates to `EntityFilterBuilder`, `SortOrderBuilder`, and `SqlLikeSearchCriteriaBuilder` enhanced their functionality.
    
    Custom JSON converters were added for better serialization and deserialization. Unit tests were added or modified to cover various scenarios, ensuring robust functionality for filtering, sorting, and querying.
    
    The project file was updated to reflect changes in dependencies and project structure, ensuring compatibility with the latest framework versions. Overall, these changes aim to improve code readability, maintainability, and performance.

--- Craft.QuerySpec/Abstractions/IEvaluator.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Craft.QuerySpec;
-
-public interface IEvaluator
-{
-    IQueryable<T>? GetQuery<T>(IQueryable<T> queryable, IQuery<T>? query) where T : class;
-}
--- Craft.QuerySpec/Enums/OrderTypeEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Craft.QuerySpec;
-
-public enum OrderTypeEnum
-{
-    OrderBy = 1,
-    OrderByDescending = 2,
-    ThenBy = 3,
-    ThenByDescending = 4
-}
