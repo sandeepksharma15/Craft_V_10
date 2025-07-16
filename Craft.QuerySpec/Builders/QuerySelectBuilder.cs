06040747ad0fe309cf166890790f65b22abd118b
Sandeep.Sharma <Sandeepksharma15@gmail.com>
2025-07-16 14:31:47 +0530

    Enhance query specification with new builder interfaces
    - Removed empty folder reference in `Craft.QuerySpec.csproj`.
    - Added `IQuerySelectBuilder<T, TResult>` and `IQuerySelectBuilder<T>` interfaces for building select expressions.
    - Introduced `QuerySelectBuilder<T>` and `QuerySelectBuilder<T, TResult>` classes to implement the new interfaces and support complex select operations.
    - Implemented `QuerySelectBuilderJsonConverter<T, TResult>` for JSON serialization of query builders.
    - Added `GetClone` method in `JsonSerializerOptionsExtensions` to clone `JsonSerializerOptions`.
    - These changes improve the structure and type safety of query select expression construction and serialization.

---
+++ Craft.QuerySpec/Builders/QuerySelectBuilder.cs
@@ -0,0 +1,176 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Craft.QuerySpec;
+
+[Serializable]
+public class QuerySelectBuilder<T> : QuerySelectBuilder<T, T>, IQuerySelectBuilder<T> where T : class;
+
+/// <summary>
+/// A builder class for constructing select expressions.
+/// </summary>
+[Serializable]
+public class QuerySelectBuilder<T, TResult> : IQuerySelectBuilder<T, TResult>
+    where T : class
+    where TResult : class
+{
+    public List<SelectDescriptor<T, TResult>> SelectDescriptorList { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuerySelectBuilder{T, TResult}"/> class.
+    /// </summary>
+    public QuerySelectBuilder() => SelectDescriptorList = [];
+
+    /// <summary>
+    /// Gets the count of select expressions.
+    /// </summary>
+    public long Count => SelectDescriptorList.Count;
+
+    public QuerySelectBuilder<T, TResult> Add(Expression<Func<T, object>> assignor, Expression<Func<TResult, object>> assignee)
+    {
+        ArgumentNullException.ThrowIfNull(assignor);
+        ArgumentNullException.ThrowIfNull(assignee);
+
+        var assignorName = assignor.GetMemberName();
+        var assigneeName = assignee.GetMemberName();
+
+        SelectDescriptorList.Add(new SelectDescriptor<T, TResult>(assignorName, assigneeName));
+
+        return this;
+    }
+
+    public QuerySelectBuilder<T, TResult> Add(SelectDescriptor<T, TResult> selectDescriptor)
+    {
+        ArgumentNullException.ThrowIfNull(selectDescriptor);
+
+        SelectDescriptorList.Add(selectDescriptor);
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a select expression.
+    /// </summary>
+    public QuerySelectBuilder<T, TResult> Add(LambdaExpression assignor, LambdaExpression assignee)
+    {
+        ArgumentNullException.ThrowIfNull(assignor);
+        ArgumentNullException.ThrowIfNull(assignee);
+
+        SelectDescriptorList.Add(new SelectDescriptor<T, TResult>(assignor, assignee));
+
+        return this;
+    }
+
+    public QuerySelectBuilder<T, TResult> Add(Expression<Func<T, object>> column)
+    {
+        ArgumentNullException.ThrowIfNull(column);
+
+        var columnName = column.GetMemberName();
+
+        SelectDescriptorList.Add(new SelectDescriptor<T, TResult>(columnName));
+
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a select expression with a single column.
+    /// </summary>
+    public QuerySelectBuilder<T, TResult> Add(LambdaExpression column)
+    {
+        ArgumentNullException.ThrowIfNull(column);
+
+        SelectDescriptorList.Add(new SelectDescriptor<T, TResult>(column));
+
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a select expression with a single column identified by its name.
+    /// </summary>
+    public QuerySelectBuilder<T, TResult> Add(string assignorPropName)
+    {
+        ArgumentNullException.ThrowIfNull(assignorPropName);
+
+        SelectDescriptorList.Add(new SelectDescriptor<T, TResult>(assignorPropName));
+
+        return this;
+    }
+
+    /// <summary>
+    /// Adds a select expression with a mapping between two properties.
+    /// </summary>
+    public QuerySelectBuilder<T, TResult> Add(string assignorPropName, string assigneePropName)
+    {
+        ArgumentNullException.ThrowIfNull(assignorPropName);
+
+        SelectDescriptorList.Add(new SelectDescriptor<T, TResult>(assignorPropName, assigneePropName));
+
+        return this;
+    }
+
+    /// <summary>
+    /// Builds the select expression.
+    /// </summary>
+    public Expression<Func<T, TResult>>? Build()
+        => typeof(TResult) == typeof(object) ? BuildAnnonymousSelect() : BuildSelect();
+
+    /// <summary>
+    /// Clears the select expressions.
+    /// </summary>
+    public void Clear()
+        => SelectDescriptorList.Clear();
+
+    private Expression<Func<T, TResult>>? BuildAnnonymousSelect()
+    {
+        if (SelectDescriptorList.Count == 0)
+            return null;
+
+        var sourceParam = Expression.Parameter(typeof(T), "x");
+
+        var selectExpressions = SelectDescriptorList.Select(item =>
+        {
+            if (item.Assignor == null)
+                throw new InvalidOperationException("Assignor expression cannot be null in BuildAnnonymousSelect");
+
+            var columnInvoke = Expression.Invoke(item.Assignor, sourceParam);
+            return Expression.Convert(columnInvoke, typeof(object));
+        });
+
+        var selectorBody = Expression.NewArrayInit(typeof(object), selectExpressions);
+
+        return Expression.Lambda<Func<T, TResult>>(
+            Expression.Convert(selectorBody, typeof(TResult)),
+            sourceParam);
+    }
+
+    private Expression<Func<T, TResult>> BuildSelect()
+    {
+        if (SelectDescriptorList.Count == 0)
+            throw new InvalidOperationException("No select mappings defined in QuerySelectBuilder.");
+
+        var selectParam = Expression.Parameter(typeof(T));
+        var memberBindings = new List<MemberBinding>();
+
+        foreach (var item in SelectDescriptorList)
+        {
+            if (item.Assignor == null)
+                throw new InvalidOperationException("Assignor expression cannot be null in BuildSelect");
+
+            var columnInvoke = Expression.Invoke(item.Assignor, selectParam);
+            var propertyInfo = item.Assignee?.GetPropertyInfo();
+            var propertyType = propertyInfo?.PropertyType;
+
+            if (propertyInfo == null || propertyType == null)
+                throw new InvalidOperationException("Property info or type cannot be null.");
+
+            var convertedValue = Expression.Convert(columnInvoke, propertyType);
+
+            var memberBinding = Expression.Bind(propertyInfo, convertedValue);
+            memberBindings.Add(memberBinding);
+        }
+
+        var memberInit = Expression.MemberInit(Expression.New(typeof(TResult)), memberBindings);
+        var selectorLambda = Expression.Lambda<Func<T, TResult>>(memberInit, selectParam);
+
+        return Expression.Lambda<Func<T, TResult>>(selectorLambda.Body, selectParam);
+    }
+}
---
+++ Craft.QuerySpec/Builders/QuerySelectBuilderJsonConverter.cs
@@ -0,0 +1,106 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Craft.QuerySpec;
+
+/// <summary>
+/// Extension methods for JsonSerializerOptions.
+/// </summary>
+public static class JsonSerializerOptionsExtensions
+{
+    /// <summary>
+    /// Creates a shallow clone of JsonSerializerOptions.
+    /// </summary>
+    /// <param name="options">The options to clone.</param>
+    /// <returns>A cloned instance of JsonSerializerOptions.</returns>
+    public static JsonSerializerOptions GetClone(this JsonSerializerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        var clone = new JsonSerializerOptions()
+        {
+            AllowTrailingCommas = options.AllowTrailingCommas,
+            DefaultBufferSize = options.DefaultBufferSize,
+            DictionaryKeyPolicy = options.DictionaryKeyPolicy,
+            DefaultIgnoreCondition = options.DefaultIgnoreCondition,
+            IgnoreReadOnlyFields = options.IgnoreReadOnlyFields,
+            IgnoreReadOnlyProperties = options.IgnoreReadOnlyProperties,
+            IncludeFields = options.IncludeFields,
+            MaxDepth = options.MaxDepth,
+            NumberHandling = options.NumberHandling,
+            PropertyNameCaseInsensitive = options.PropertyNameCaseInsensitive,
+            PropertyNamingPolicy = options.PropertyNamingPolicy,
+            ReadCommentHandling = options.ReadCommentHandling,
+            WriteIndented = options.WriteIndented
+        };
+
+        // Copy converters
+        foreach (var converter in options.Converters)
+        {
+            clone.Converters.Add(converter);
+        }
+
+        return clone;
+    }
+}
+
+public class QuerySelectBuilderJsonConverter<T, TResult> : JsonConverter<QuerySelectBuilder<T, TResult>>
+    where T : class
+    where TResult : class
+{
+    public override bool CanConvert(Type objectType)
+        => objectType == typeof(QuerySelectBuilder<T, TResult>);
+
+    public override QuerySelectBuilder<T, TResult> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var querySelectBuilder = new QuerySelectBuilder<T, TResult>();
+
+        // We Want To Clone The Options To Add The SelectDescriptorJsonConverter
+        var localOptions = options.GetClone();
+        localOptions.Converters.Add(new SelectDescriptorJsonConverter<T, TResult>());
+
+        // Check for array start
+        if (reader.TokenType != JsonTokenType.StartArray)
+            throw new JsonException("Invalid format for QuerySelectBuilder: expected array of SelectDescriptor");
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndArray)
+                break;
+
+            // Read the individual Select Descriptor object
+            var selectDescriptor = JsonSerializer.Deserialize<SelectDescriptor<T, TResult>>(ref reader, localOptions);
+
+            // Validate and add the SelectDescriptor
+            if (selectDescriptor != null)
+                querySelectBuilder.Add(selectDescriptor);
+            else
+                throw new JsonException("Invalid select descriptor encountered in QuerySelectBuilder array");
+        }
+
+        return querySelectBuilder;
+    }
+
+    public override void Write(Utf8JsonWriter writer, QuerySelectBuilder<T, TResult> value, JsonSerializerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(writer);
+        ArgumentNullException.ThrowIfNull(value);
+        ArgumentNullException.ThrowIfNull(options);
+
+        // Start The Array
+        writer.WriteStartArray();
+
+        // We Want To Clone The Options To Add The SelectDescriptorJsonConverter
+        var localOptions = options.GetClone();
+        localOptions.Converters.Add(new SelectDescriptorJsonConverter<T, TResult>());
+
+        foreach (var selectDescriptor in value.SelectDescriptorList)
+        {
+            JsonSerializer.Serialize(writer, selectDescriptor, localOptions);
+        }
+
+        // End the array
+        writer.WriteEndArray();
+    }
+}
