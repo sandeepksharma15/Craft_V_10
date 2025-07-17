1d6bbcd5c3eef1f3613fc2c621cdc80127473328
Sandeep.Sharma <Sandeepksharma15@gmail.com>
2025-07-17 11:31:31 +0530

    Enhance entity querying with new interfaces and methods
    This commit introduces several new interfaces and methods in the `Craft.QuerySpec` namespace for improved querying and handling of entities.
    
    Key changes include:
    - Updated test case in `PageResponseJsonConverterFactoryTests.cs` for better readability.
    - Added and modified interfaces: `IEntityController`, `IEvaluator`, `IHttpService`, `IQuery`, `IQueryJsonConverter`, `IRepository`, and `ISelectEvaluator`.
    - New methods for asynchronous operations, pagination, and filtering/sorting capabilities.
    - Enhanced structure for managing entity data, including support for cancellation tokens.
    
    These changes improve the functionality and usability of the codebase for developers working with entity queries and responses.

---
+++ Craft.QuerySpec/Abstractions/IEvaluator.cs
@@ -0,0 +1,6 @@
+﻿namespace Craft.QuerySpec;
+
+public interface IEvaluator
+{
+    IQueryable<T>? GetQuery<T>(IQueryable<T> queryable, IQuery<T>? query) where T : class;
+}
---
+++ Craft.QuerySpec/Abstractions/IQuery.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+
+namespace Craft.QuerySpec;
+
+public interface IQuery<T, TResult> : IQuery<T>
+    where T : class
+    where TResult : class
+{
+    new Func<IEnumerable<TResult>, IEnumerable<TResult>>? PostProcessingAction { get; internal set; }
+
+    QuerySelectBuilder<T, TResult>? QuerySelectBuilder { get; }
+
+    Expression<Func<T, IEnumerable<TResult>>>? SelectorMany { get; internal set; }
+
+    new void Clear();
+}
+
+public interface IQuery<T> where T : class
+{
+    bool AsNoTracking { get; internal set; }
+    bool AsSplitQuery { get; internal set; }
+    bool IgnoreAutoIncludes { get; internal set; }
+    bool IgnoreQueryFilters { get; internal set; }
+
+    int? Skip { get; set; }
+    int? Take { get; set; }
+
+    SortOrderBuilder<T>? SortOrderBuilder { get; internal set; }
+    Func<IEnumerable<T>, IEnumerable<T>>? PostProcessingAction { get; internal set; }
+    SqlLikeSearchCriteriaBuilder<T>? SqlLikeSearchCriteriaBuilder { get; internal set; }
+    EntityFilterBuilder<T>? EntityFilterBuilder { get; internal set; }
+
+    void Clear();
+    bool IsSatisfiedBy(T entity);
+    void SetPage(int page, int pageSize);
+}
