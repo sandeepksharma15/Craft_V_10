258353ff0420c26b177c9ca3a84b3d83e5cb655f
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-07-21 10:17:24 +0530

    Add abstract ApiGetRequest classes for API GET handling
    Introduce `ApiGetRequest<TKEY>` and `ApiGetRequest` classes
    to manage API GET requests with properties for request type
    and detail inclusion. Implement JSON polymorphism for
    serialization of derived request types.

---
+++ Craft.Core/Request/ApiGetRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Craft.Core.Request;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
+[JsonDerivedType(typeof(GetRequest), "get")]
+[JsonDerivedType(typeof(GetAllRequest), "getAll")]
+[JsonDerivedType(typeof(GetPagedRequest), "getPaged")]
+public abstract class ApiGetRequest<TKEY>
+{
+    public ApiRequestType RequestType { get; }
+    public bool IncludeDetails { get; set; } = false;
+
+    protected ApiGetRequest(ApiRequestType requestType)
+    {
+        RequestType = requestType;
+    }
+}
+
+public abstract class ApiGetRequest : ApiGetRequest<KeyType>
+{
+    protected ApiGetRequest(ApiRequestType requestType) : base(requestType) { }
+}
