4c01e1eb5dd5e5d96c794650850be47a55d03117
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-08-14 17:23:00 +0530

    Add connection string security and validation features
    Introduce `ConnectionStringSecurer` and `ConnectionStringValidator` classes for securing and validating connection strings for PostgreSQL and SQL Server. Implement `CustomSeederRunner` to manage seeders asynchronously. Update `DatabaseOptions` to support validation of configuration properties. Add necessary using directives for new functionality.

---
+++ Craft.Data/Helpers/CustomSeederRunner.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Craft.Data.Helpers;
+
+public class CustomSeederRunner(IServiceProvider serviceProvider)
+{
+    private readonly ICustomSeeder[] _seeders = [.. serviceProvider.GetServices<ICustomSeeder>()];
+
+    public async Task RunSeedersAsync(CancellationToken cancellationToken)
+    {
+        foreach (var seeder in _seeders)
+            await seeder.InitializeAsync(cancellationToken);
+    }
+}
---
+++ Craft.Data/Options/DatabaseOptions.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Craft.Data;
+
+public class DatabaseOptions : IValidatableObject
+{
+    public const string SectionName = "DatabaseOptions";
+
+    public int CommandTimeout { get; set; } = 30;
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DbProvider { get; set; } = string.Empty;
+    public bool EnableDetailedErrors { get; set; }
+    public bool EnableSensitiveDataLogging { get; set; }
+    public int MaxRetryCount { get; set; } = 3;
+    public int MaxRetryDelay { get; set; } = 15;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (DbProvider.IsNullOrEmpty())
+            yield return new ValidationResult(
+                $"{nameof(DatabaseOptions)}.{nameof(DbProvider)} is not configured",
+                [nameof(DbProvider)]);
+
+        if (ConnectionString.IsNullOrEmpty())
+            yield return new ValidationResult(
+                $"{nameof(DatabaseOptions)}.{nameof(ConnectionString)} is not configured",
+                [nameof(ConnectionString)]);
+    }
+}
