295c737079ad459124529b1fa29fd70c88062cf3
Sandeep.Sharma <Sandeepksharma15@gmail.com>
2025-12-16 11:35:23 +0530

    Add CraftComponent base class and update project structure
    - Introduced abstract CraftComponent base class for Blazor UI components, standardizing common parameters and behaviors.
    - Updated Craft.UiConponents.csproj to include wwwroot folder and add references to Craft.Extensions and Craft.Utilities.
    - Removed unused background.png and exampleJsInterop.js files.

---
+++ 1. Source/7. UI/Craft.UiConponents/Base/CraftComponent.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Craft.UiConponents;
+
+public abstract class CraftComponent : ComponentBase
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public string? Class { get; set; }
+    [Parameter] public ElementReference ElementRef { get; set; }
+    [Parameter] public Action<ElementReference>? ElementRefChanged { get; set; }
+    [Parameter] public string? Id { get; set; }
+    [Parameter] public string? Style { get; set; }
+    [Parameter] public object? Tag { get; set; }
+    [Parameter(CaptureUnmatchedValues = true)] public IDictionary<string, object>? UserAttributes { get; set; }
+    [Parameter] public virtual bool Visible { get; set; } = true;
+
+    protected override void OnInitialized()
+    {
+        Id ??= Guid.NewGuid().ToString("N")[..10];
+    }
+
+    /// <summary>
+    /// Gets the component's ID, prioritizing the user-defined "id" attribute over the default Id property.
+    /// </summary>
+    /// <returns>The component's ID as a string.</returns>
+    protected internal string GetId()
+    {
+        if (UserAttributes?.TryGetValue("id", out var id) == true)
+        {
+            var idString = Convert.ToString(id);
+
+            if (!idString.IsNullOrEmpty())
+                return idString!;
+        }
+
+        return Id ?? string.Empty;
+    }
+
+    public override async Task SetParametersAsync(ParameterView parameters)
+    {
+        parameters.TryGetValue(nameof(Visible), out bool visibleState);
+
+        await base.SetParametersAsync(parameters);
+
+        if (Visible != visibleState)
+            StateHasChanged();
+    }
+}
