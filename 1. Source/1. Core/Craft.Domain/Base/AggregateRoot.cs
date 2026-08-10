dc0e6640f7d494f3f4b0d3849457a1669cac7b83
sandeepksharma15 <sandeepksharma15@gmail.com>
2026-08-10 16:34:35 +0530

    refactor: domain base classes, Msisdn rename, add Empty
    Refactored ByteArrayValueObject<TSelf>.Equals for clarity. Renamed MsIsdn to Msisdn throughout MsIsdn.cs. Added static Empty instances to CommandData, RawMessage, and RawPacket. Made BaseEntity<TKey> implement DomainObject. Introduced AggregateRoot<TKey> base class in Craft.Domain.

---
+++ 1. Source/1. Core/Craft.Domain/Base/AggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace Craft.Domain;
+
+/// <summary> 
+/// Base type for aggregate roots. 
+/// </summary> 
+public abstract class AggregateRoot<TKey> : BaseEntity<TKey> 
+{ 
+    protected AggregateRoot() { } 
+
+    protected AggregateRoot(TKey id) : base(id) { } 
+}
